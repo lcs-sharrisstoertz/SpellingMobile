c81adb38adb89a6984db2579df439e53136bab2c
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2025-05-05 12:03:05 -0400

    Written the basic shell of the filtering function.

---
+++ SpellingMobile/Functions/Filtering.swift
@@ -0,0 +1,25 @@
+//
+//  Filtering.swift
+//  SpellingMobile
+//
+//  Created by Skye Willow Harris-Stoertz on 2025-05-05.
+//
+
+// Define the function
+//
+// name: filtering
+//
+// inputs (parameters):
+//
+//          the original array (originalList)
+//          what to filter on (desiredOutcome)
+//
+// output (return type): an array filtered based on outcome
+//
+func filtering(
+    originalList: [Result],
+    on desiredOutcome: Outcome
+) -> [Outcome] {
+    // return and empty array
+    return []
+}
